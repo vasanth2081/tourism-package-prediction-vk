43ae35c6a9ba29a872c06ab26e1410232336bba8
vasanth2081 <er_vasanth@yahoo.co.in>
2026-03-28 19:59:46 +0000

    first commit 28032026

--- week_3_mls/hosting/hosting.py
+++ week_3_mls/hosting/hosting.py
@@ -3,7 +3,7 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="/content/drive/My Drive/PGP GL/Advanced ML/week_3_mls/deployment",     # the local folder containing your files
+    folder_path="week_3_mls/deployment",     # the local folder containing your files
     repo_id="v-vasanth2009/tourism-package-prediction-28032026",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
