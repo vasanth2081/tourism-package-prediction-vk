dc3e6b51ca6e511480be72123c745df6eb829f9f
vasanth2081 <er_vasanth@yahoo.co.in>
2026-03-28 19:06:45 +0000

    first commit 28032026

--- week_3_mls/model_building/data_register.py
+++ week_3_mls/model_building/data_register.py
@@ -3,7 +3,7 @@ from huggingface_hub import HfApi, create_repo
 import os
 
 
-repo_id = "vasanth2081/tourism-package-prediction-28032026"
+repo_id = "v-vasanth2009/tourism-package-prediction-28032026"
 repo_type = "dataset"
 
 # Initialize API client
