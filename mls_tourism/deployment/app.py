d4c1f8727286e73be5244c75cd2e76a97b7489a9
vasanth2081 <er_vasanth@yahoo.co.in>
2026-03-29 07:27:02 +0000

    first commit 29032026

---
+++ mls_tourism/deployment/app.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+from huggingface_hub import hf_hub_download
+import joblib
+
+# Download and load the model
+model_path = hf_hub_download(repo_id="v-vasanth2009/tourism-package-prediction-28032026", filename="best_tourism_prediction_model_v1.joblib")
+model = joblib.load(model_path)
+
+# Streamlit UI for Machine Failure Prediction
+st.title("Tourism Package Prediction App")
+st.write("""
+This application predicts the likelihood of a Customer purchasing Wellness package based on customer and interaction data.
+Please enter the Customer and interaction data below to get a prediction.
+""")
+
+# User input
+Age = st.number_input("Age", min_value=0, max_value=100, value=25)
+TypeofContact = st.selectbox("Type of Contact", ["Company Invited", "Self Enquiry"])
+CityTier = st.number_input("City Tier", min_value=1, max_value=3, value=2)
+DurationOfPitch = st.number_input("Duration of Pitch (min)", min_value=0, max_value=3000, value=1, step=1)
+Occupation = st.selectbox("Occupation", ["Free Lancer", "Large Business", "Small Business", "Salaried"])
+Gender = st.selectbox("Gender", ["Male", "Female"])
+NumberOfPersonVisiting = st.number_input("Number of Person Visiting", min_value=1, max_value=5, value=1, step=1)
+NumberOfFollowups = st.number_input("Number of Followups", min_value=1, max_value=6, value=1, step=1)
+ProductPitched = st.selectbox("Product Pitched", ["Deluxe", "Basic", "King", "Standard", "Super Deluxe"])
+PreferredPropertyStar = st.number_input("Preferred Property Star", min_value=1, max_value=5, value=3)
+MaritalStatus = st.selectbox("Marital Status", ["Married", "Single", "Divorced", "Unmarried"])
+NumberOfTrips = st.number_input("Number of Trips", min_value=1, max_value=25, value=1, step=1)
+Passport = st.selectbox("Passport", ["0", "1"])
+PitchSatisfactionScore = st.number_input("Pitch Satisfaction Score", min_value=1, max_value=5, value=3)
+OwnCar = st.selectbox("Own Car", ["0", "1"])
+NumberOfChildrenVisiting = st.number_input("Number of Children Visiting",min_value=1, max_value=5, value=3)
+Designation = st.selectbox("Designation", ["Executive", "AVP", "Manager", "Senior Manager", "VP"])
+MonthlyIncome = st.number_input("Monthly Income", min_value=1000, max_value=500000, value=30000, step=1000)
+
+# Assemble input into DataFrame
+input_data = pd.DataFrame([{
+    'Age': Age,
+    'TypeofContact': TypeofContact,
+    'CityTier': CityTier,
+    'DurationOfPitch': DurationOfPitch,
+    'Occupation': Occupation,
+    'Gender': Gender,
+    'NumberOfPersonVisitingge': NumberOfPersonVisiting,
+    'NumberOfFollowups': NumberOfFollowups,
+    'ProductPitched': ProductPitched,
+    'PreferredPropertyStar': PreferredPropertyStar,
+    'MaritalStatus': MaritalStatus,
+    'NumberOfTrips': NumberOfTrips,
+    'Passport': Passport,
+    'PitchSatisfactionScore': PitchSatisfactionScore,
+    'OwnCar': OwnCar,
+    'NumberOfChildrenVisiting': NumberOfChildrenVisiting,
+    'Designation': Designation,
+    'MonthlyIncome': MonthlyIncome
+}])
+
+
+if st.button("Predict Package Purchase"):
+    prediction = model.predict(input_data)[0]
+    result = "Package Purchased" if prediction == 1 else "Package Not Purchased"
+    st.subheader("Prediction Result:")
+    st.success(f"The model predicts: **{result}**")
---
+++ mls_tourism/model_building/prep.py
@@ -0,0 +1,64 @@
+# for data manipulation
+import pandas as pd
+import sklearn
+# for creating a folder
+import os
+# for data preprocessing and pipeline creation
+from sklearn.model_selection import train_test_split
+# for converting text data in to numerical representation
+from sklearn.preprocessing import LabelEncoder
+# for hugging face space authentication to upload files
+from huggingface_hub import login, HfApi
+
+# Define constants for the dataset and output paths
+api = HfApi(token=os.getenv("HF_TOKEN"))
+DATASET_PATH = "hf://datasets/v-vasanth2009/tourism-package-prediction-28032026/tourism.csv"
+df = pd.read_csv(DATASET_PATH)
+print("Dataset loaded successfully.")
+
+# Handle Missing Values
+for col in df.columns:
+    if df[col].dtype == 'object':
+        df[col].fillna(df[col].mode()[0], inplace=True)
+    else:
+        df[col].fillna(df[col].median(), inplace=True)
+
+# Drop the unnammed : 0 column
+if 'Unnamed: 0' in df.columns:
+    df.drop(columns=['Unnamed: 0'], inplace=True)
+
+# Drop the unique identifier
+df.drop(columns=['CustomerID'], inplace=True)
+
+# Encoding the categorical 'Type' column
+label_encoder = LabelEncoder()
+#df['Type'] = label_encoder.fit_transform(df['Type'])
+for col in df.select_dtypes(include='object').columns:
+    df[col] = label_encoder.fit_transform(df[col])
+
+target_col = 'ProdTaken'
+
+# Split into X (features) and y (target)
+X = df.drop(columns=[target_col])
+y = df[target_col]
+
+# Perform train-test split
+Xtrain, Xtest, ytrain, ytest = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+Xtrain.to_csv("Xtrain.csv",index=False)
+Xtest.to_csv("Xtest.csv",index=False)
+ytrain.to_csv("ytrain.csv",index=False)
+ytest.to_csv("ytest.csv",index=False)
+
+
+files = ["Xtrain.csv","Xtest.csv","ytrain.csv","ytest.csv"]
+
+for file_path in files:
+    api.upload_file(
+        path_or_fileobj=file_path,
+        path_in_repo=file_path.split("/")[-1],  # just the filename
+        repo_id="v-vasanth2009/tourism-package-prediction-28032026",
+        repo_type="dataset",
+    )
